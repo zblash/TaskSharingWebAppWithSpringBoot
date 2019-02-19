e796b63a48ce78bf40afe98011d0dc5a3040a3c5
Yusuf Can Celik <yusufcancelik@hotmail.com>
2019-02-19 22:33:39 +0300

    Application class name changed

--- src/test/java/com/tasksharing/tasksharing/tasksharingApplicationTests.java
+++ src/test/java/com/tasksharing/tasksharing/tasksharingApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ApiApplicationTests {
+public class tasksharingApplicationTests {
 
 	@Test
 	public void contextLoads() {
